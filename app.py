d0e28df9b33508eb2837d02c026559d4004d6134
Joshua Dale <daleyahooatom@gmail.com>
2022-11-30 22:00:16 +0800

    Initialized more

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return '<h1> Hello!<h1>'
+def Hometest():
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
